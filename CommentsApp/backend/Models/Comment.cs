696c2c2b0acd49a73e0c7c10d6f1fefb2bce38c4
Yevhen Kazanzhy <z.kazanzhi2017@gmail.com>
2025-09-17 13:48:31 +0300

    add Comment model

---
+++ CommentsApp/backend/Models/Comment.cs
@@ -0,0 +1,17 @@
+﻿namespace backend.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public int? ParentId { get; set; }
+        public Comment Parent { get; set; }
+        public ICollection<Comment> Replies { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Text { get; set; }
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public string FilePath { get; set; }
+        public string FileType { get; set; }
+        public int? FileSize { get; set; }
+    }
+}
